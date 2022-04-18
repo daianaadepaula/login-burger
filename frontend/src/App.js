532cac6a8e410c74b62fa976b85e5274fab6c7a7
Daiana de Paula <daianaadepaula1@gmail.com>
2022-04-18 09:17:17 -0300

    styling order card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,57 @@
 import React from "react";
 
 import Burger from './assets/burger.svg';
+import Trash from './assets/trash.svg';
 
-import { 
+import {
   Container,
   Image,
   H1,
   ContainerItens,
   InputLabel,
   Input,
-  Button
+  Button,
+  Order,
 } from "./styles";
 
 const App = () => {
 
+  const orders = [
+    { id: Math.random(), order: "X-Salada", name: "Daiana" },
+    { id: Math.random(), order: "1 Batata Grande, 1 X-Bacon,   2 Coca-Colas Light", name: "Maria" },
+  ];
+
   return (
-      
+
     <Container>
       <Image alt="logo" src={Burger} />
       <H1>Faça seu pedido!</H1>
 
-        <ContainerItens>
-          
-          <InputLabel>Pedido</InputLabel>
-          <Input placeholder='Pedido' />
+      <ContainerItens>
 
-          <InputLabel>Nome do Cliente</InputLabel>
-          <Input placeholder='Nome do cliente' />
+        <InputLabel>Pedido</InputLabel>
+        <Input placeholder='Pedido' />
 
-          <Button>Novo Pedido</Button>
+        <InputLabel>Nome do Cliente</InputLabel>
+        <Input placeholder='Nome do cliente' />
 
-        </ContainerItens>
+        <Button>Novo Pedido</Button>
+
+        <ul>
+          {orders.map((demand) => (
+            <Order key={demand.id}>
+              <div>
+                <p>{demand.order}</p>
+                <p className="order-name" >{demand.name}</p>
+              </div>
+              <button>
+                <img src={Trash} alt="lata-de-lixo" />
+              </button>
+            </Order>
+          ))}
+        </ul>
+
+      </ContainerItens>
 
     </Container>
 
--- frontend/src/styles.js
+++ frontend/src/styles.js
@@ -32,6 +32,8 @@ export const H1 = styled.h1`
 export const ContainerItens = styled.div`
     display: flex;
     flex-direction: column;
+
+    height: 100vh;
 `
 
 export const InputLabel = styled.p`
@@ -89,5 +91,42 @@ export const Button = styled.button`
 
     &:active{
         opacity: 0.5;
+    }
+`
+
+export const Order = styled.li`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 25px;
+    margin-top: 20px;
+
+    width: 342px;
+    height: 101px;
+    background: rgba(255, 255, 255, 0.25);
+    border-radius: 14px;
+    color: #FFFFFF;
+    border: none;
+    outline: none;
+
+    font-style: normal;
+    font-weight: 300;
+    font-size: 18px;
+    line-height: 21px;
+
+    div{
+        flex-direction: column;
+        justify-content: space-between;
+        
+    }
+
+    .order-name{
+        font-weight: 700;
+    }
+
+    button{
+        background: none;
+        border: none;
+        cursor: pointer;
     }
 `
